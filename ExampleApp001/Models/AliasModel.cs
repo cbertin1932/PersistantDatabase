b6fb960af8717ed710ca219d5062855beab9b208
cbertin1932 <cbertin1932@yahoo.com>
2017-05-01 12:22:33 -0400

    Add files via upload

---
+++ ExampleApp001/Models/AliasModel.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ExampleApp001.Models
+{
+
+    public class AliasModel
+    {
+        public string Name { get; set; }
+        public string Alias { get; set; }
+    }
+
+}
---
+++ ExampleApp001/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExampleApp001.Startup))]
+namespace ExampleApp001
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
